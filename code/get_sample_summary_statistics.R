3ee9f489eabfe13c9e3c67efdb4244637c084ef1
pschloss <pschloss@umich.edu>
2019-11-05 11:19:23 -0500

    Generate data and viz for first results paragraph

---
+++ code/get_sample_summary_statistics.R
@@ -0,0 +1,32 @@
+library(tidyverse)
+
+summarize_data <- function(environment) {
+
+	removed_samples <- paste0("data/", environment, "/data.remove_accnos") %>%
+		read_lines(.) %>%
+		str_split("-") %>%
+		unlist() %>%
+		enframe(., name=NULL, value="sample")
+
+	paste0("data/", environment, "/data.count.summary") %>%
+		read_tsv(col_names=c("sample", "n_seqs"), col_types=cols(sample=col_character(), n_seqs=col_integer())) %>%
+		anti_join(., removed_samples, by="sample") %>%
+		summarize(n_samples=n(), total_seqs=sum(n_seqs), mean=mean(n_seqs),
+			min=min(n_seqs), l_quartile = quantile(n_seqs, 0.25), median=median(n_seqs),
+			u_quartile = quantile(n_seqs, 0.75), max=max(n_seqs),
+			n_removed = removed_samples %>% filter(sample != "") %>% nrow(),
+			fold_difference = max/min)
+}
+
+
+tibble(directory=c("bioethanol", "human", "lake", "marine", "mice", "rainforest",
+									"rice", "seagrass", "sediment", "soil", "stream"),
+			nice_name=c("Bioethanol", "Human", "Lake", "Marine", "Mice", "Rainforest",
+									"Rice", "Seagrass", "Sediment", "Soil", "Stream"),
+			reference=rep("", 11)
+		) %>%
+		group_by(directory) %>%
+		mutate(summary_data = map(directory, summarize_data)) %>%
+		unnest(summary_data) %>%
+		ungroup() %>%
+		write_tsv("data/process/study_summary_statistics.tsv")
---
+++ code/plot_seqs_per_sample.R
@@ -0,0 +1,52 @@
+library(tidyverse)
+
+set.seed(19760620)
+
+get_removed_samples <- function(environment) {
+
+	paste0("data/", environment, "/data.remove_accnos") %>%
+		read_lines(.) %>%
+		str_split("-") %>%
+		unlist() %>%
+		enframe(., name=NULL, value="sample") %>%
+		mutate(study = environment)
+
+}
+
+
+studies <- c("bioethanol", "human", "lake", "marine", "mice", "rainforest",
+									"rice", "seagrass", "sediment", "soil", "stream")
+
+nice_studies <- c("Bioethanol", "Human", "Lake", "Marine", "Mice", "Rainforest",
+						"Rice", "Seagrass", "Sediment", "Soil", "Stream")
+names(nice_studies) <- studies
+
+removed_samples <- studies %>%
+	map_df(., get_removed_samples) %>%
+	filter(sample != "") %>%
+	mutate(status = "removed")
+
+
+counts_data <- tibble(study=studies,
+			file_name=paste0("data/", study, "/data.count.summary")) %>%
+	group_by(file_name) %>%
+	nest() %>%
+	mutate(count_data = map(file_name, ~read_tsv(file=.x, col_names=c("sample", "n_seqs"), col_types=cols(sample=col_character(), n_seqs=col_integer())))) %>%
+	ungroup() %>%
+	select(-file_name) %>%
+	unnest(cols=c("data", "count_data")) %>%
+	full_join(., removed_samples, by=c("study", "sample")) %>%
+	mutate(status = replace_na(status, "kept")) %>%
+	mutate(study = factor(study, sort(studies, decreasing=T)))
+
+ggplot(counts_data, aes(y=n_seqs, x=study)) +
+	geom_vline(xintercept=seq(1:11), col="gray") +
+	geom_jitter(width=0.2, pch=19, alpha=0.33, aes(color=status)) +
+	scale_y_log10(limits=c(0.9, 1e6), breaks=c(1, 10, 100, 1000, 10000, 100000, 1e6), labels=c("1", "10", "100", "1,000", "10,000", "100,000", "1,000,000")) +
+	scale_x_discrete(breaks=studies, labels=nice_studies[studies]) +
+	scale_color_manual(name=NULL, values=c("black", "red"), breaks=c("kept", "removed"), labels=c("Kept", "Removed"))+
+	coord_flip() +
+	labs(y="Number of sequences per sample", x=NULL) +
+	theme_classic()
+
+ggsave("results/figures/seqs_per_sample.pdf", width=7, height=4)
